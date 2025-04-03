bb26a3759abd6eea8549eb8735a9cbe58a91a7fa
Marco Robol <marco.robol@unitn.it>
2025-04-03 09:08:25 +0200

    First commit

---
+++ array.js
@@ -0,0 +1,13 @@
+var list = ["apple", "pear", "peach"]; // list of elements
+list[0] // accessing an element by id
+list.indexOf("pear") // checking the index of "pear" in the array
+list.push("banana"); // Adding a new element
+list.pop() // Taking the last element from the array
+list.shift() // Taking the first element
+list.length // checking the number of elements
+// list.slice(start, end) // copy a subportion of the original array
+list.join('separator') // return string by concatening elements
+
+// list.forEach( console.log ) // array methods
+list.forEach( v => console.log(v) )
+console.log( list.join(', ') );
---
+++ fn.js
@@ -0,0 +1,25 @@
+/**
+ * Funzione che somma due numeri
+ * @param {Number} a numero da moltiplicare
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function add( a, b ) { // Function Declaration
+    return a + b;
+};
+
+var mult = function( a, b ) { // Function Expression
+    return a * b;
+};
+
+const mod = ( a, b ) => { // Arrow Function (+args +instructions)
+    let r = a % b;
+    return r;
+}
+
+var pow2 = a => a * a // Arrow Function (1arg 1instruction, no return needed)
+
+console.log( add('s',2) );
+console.log( mult(2,3) );
+console.log( mod(3,2) );
+console.log( pow2(3) );
